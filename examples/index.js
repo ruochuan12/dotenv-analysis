7f25851effe3e6035daa5a0924255e2424dbdee9
若川 <lxchuan12@163.com>
2021-12-22 00:00:28 +0800

    feat: 🎸 初步撰写文章

--- examples/index.js
+++ examples/index.js
@@ -1,35 +1,3 @@
-# dotenv-analysis
-
-## 前言
-
-## dotenv 的作用
-
-[dotenv](https://github.com/motdotla/dotenv)
-
-[dotenv-expand](https://github.com/motdotla/dotenv-expand)
-
-.env
-
-[vue-cli .env](https://cli.vuejs.org/zh/guide/mode-and-env.html#%E7%8E%AF%E5%A2%83%E5%8F%98%E9%87%8F)
-
-[create-react-app .env](https://create-react-app.dev/docs/adding-custom-environment-variables/#what-other-env-files-can-be-used)
-
-
-## 
-
-
-## 
-
-```.env
-NAME=若川
-AGE=18
-BLOG=https://lxchuan12.gitee.io
-MP_WEIXIN='若川视野'
-```
-
-### 简单实现
-
-```js
 const fs = require('fs')
 const path = require('path')
 
@@ -62,4 +30,5 @@ console.log(process.env);
 
 module.exports.config = config;
 module.exports.config = parse;
-```
+
+
